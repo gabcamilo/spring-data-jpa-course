ab1dbc278ca652adcfb2af44445df82751142f6c
Gabriela Carvalho Camilo <gabccamilo.dev@gmail.com>
2021-01-03 21:06:30 -0300

    Adding sequence generation to Student id

--- src/main/java/com/example/demo/Student.java
+++ src/main/java/com/example/demo/Student.java
@@ -2,10 +2,21 @@ package com.example.demo;
 
 import javax.persistence.*;
 
+import static javax.persistence.GenerationType.*;
+
 @Entity(name = "Student")
 public class Student {
 
     @Id
+    @SequenceGenerator(
+            name = "student_sequence",
+            sequenceName = "student_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = SEQUENCE,
+            generator = "student_sequence"
+    )
     private Long id;
     private String firstName;
     private String lastName;
