c302ddad761408ea11ac6cca2203f62688a696ef
Gabriela Carvalho Camilo <gabccamilo.dev@gmail.com>
2021-01-03 21:55:16 -0300

    Customizing unique constraint name on email at Student entity

--- src/main/java/com/example/demo/Student.java
+++ src/main/java/com/example/demo/Student.java
@@ -5,6 +5,12 @@ import javax.persistence.*;
 import static javax.persistence.GenerationType.*;
 
 @Entity(name = "Student")
+@Table(
+        name = "student",
+        uniqueConstraints = {
+                @UniqueConstraint(name = "student_email_unique", columnNames = "email")
+        }
+)
 public class Student {
 
     @Id
@@ -40,8 +46,7 @@ public class Student {
     @Column(
             name = "email",
             nullable = false,
-            columnDefinition = "TEXT",
-            unique = true
+            columnDefinition = "TEXT"
     )
     private String email;
 
