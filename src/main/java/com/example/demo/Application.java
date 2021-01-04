7076fa7f9826bf73c7718d16cab990331f360f8e
Gabriela Carvalho Camilo <gabccamilo.dev@gmail.com>
2021-01-03 23:04:22 -0300

    Adding database insertion on initialization to test student repository

--- src/main/java/com/example/demo/Application.java
+++ src/main/java/com/example/demo/Application.java
@@ -1,13 +1,29 @@
 package com.example.demo;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    /*Code running after the application started*/
+    @Bean
+    CommandLineRunner commandLineRunner(StudentRepository studentRepository) {
+        return args -> {
+            Student maria = new Student(
+                    "Maria",
+                    "Jones",
+                    "mariajones@gmail.com",
+                    22
+            );
+            studentRepository.save(maria);
+        };
     }
 
 }
