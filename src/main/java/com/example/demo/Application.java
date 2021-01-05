1aa09be5461a411395616a8eece6038db532188e
Gabriela Carvalho Camilo <gabccamilo.dev@gmail.com>
2021-01-04 23:40:14 -0300

    Adding Faker to generate random students

--- src/main/java/com/example/demo/Application.java
+++ src/main/java/com/example/demo/Application.java
@@ -1,9 +1,12 @@
 package com.example.demo;
 
+import com.github.javafaker.Faker;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+
+import java.util.List;
 
 @SpringBootApplication
 public class Application {
@@ -15,15 +18,22 @@ public class Application {
     /*Code running after the application started*/
     @Bean
     CommandLineRunner commandLineRunner(StudentRepository studentRepository) {
-        return args -> {
-            Student maria = new Student(
-                    "Maria",
-                    "Jones",
-                    "mariajones@gmail.com",
-                    22
-            );
-            studentRepository.save(maria);
-        };
+        return args -> generateRandomStudents(studentRepository);
+    }
+
+    private void generateRandomStudents(StudentRepository studentRepository) {
+        Faker faker = new Faker();
+        for (int i = 0; i < 20; i++) {
+            String firstName = faker.name().firstName();
+            String lastName = faker.name().lastName();
+            String email = String.format("%s.%s@amigoscode.edu", firstName, lastName);
+            Student student = new Student(
+                    firstName,
+                    lastName,
+                    email,
+                    faker.number().numberBetween(17, 55));
+            studentRepository.save(student);
+        }
     }
 
 }
