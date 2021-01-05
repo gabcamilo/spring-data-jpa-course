fe06f62f5514fb5e2f892cbbc8f4f7bccbea20e6
Gabriela Carvalho Camilo <gabccamilo.dev@gmail.com>
2021-01-05 00:52:36 -0300

    Adding StudentId entity

---
+++ src/main/java/com/example/demo/StudentIdCard.java
@@ -0,0 +1,63 @@
+package com.example.demo;
+
+import javax.persistence.*;
+
+import static javax.persistence.GenerationType.SEQUENCE;
+
+@Entity(name = "StudentIdCard")
+@Table(
+        name = "student_id_card",
+        uniqueConstraints = {
+                @UniqueConstraint(
+                        name = "student_id_card_number_unique",
+                        columnNames = "card_number")
+        }
+)
+public class StudentIdCard {
+
+    @Id
+    @SequenceGenerator(
+            name = "student_card_id_sequence",
+            sequenceName = "student_card_id_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = SEQUENCE,
+            generator = "student_card_id_sequence"
+    )
+    @Column(
+            name = "id",
+            updatable = false
+    )
+    private Long id;
+
+    @Column(
+            name = "card_number",
+            nullable = false,
+            columnDefinition = "TEXT"
+    )
+    private String cardNumber;
+
+    public StudentIdCard() {
+    }
+
+    public StudentIdCard(String cardNumber) {
+        this.cardNumber = cardNumber;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getCardNumber() {
+        return cardNumber;
+    }
+
+    public void setCardNumber(String cardNumber) {
+        this.cardNumber = cardNumber;
+    }
+}
