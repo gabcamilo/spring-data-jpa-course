fdad613a5171d433a7a52fe377f8450d786ab206
Gabriela Carvalho Camilo <gabccamilo.dev@gmail.com>
2021-01-05 20:43:48 -0300

    Adding embeddable id and link entity Enrollment to allow enrolment table to have two pks as it's on the diagram

---
+++ src/main/java/com/example/demo/Enrollment.java
@@ -0,0 +1,54 @@
+package com.example.demo;
+
+import javax.persistence.*;
+
+@Entity(name = "Enrollment")
+@Table(name = "enrollment")
+public class Enrollment {
+
+    @EmbeddedId
+    private EnrollmentId id;
+
+    @ManyToOne
+    @MapsId("studentId")
+    @JoinColumn(name = "student_id") //this is the actual property in the database
+    private Student student;
+
+
+    @ManyToOne
+    @MapsId("courseId")
+    @JoinColumn(name = "course_id")
+    private Course course;
+
+    public Enrollment() {
+    }
+
+    public Enrollment(Student student, Course course) {
+        this.student = student;
+        this.course = course;
+    }
+
+    public EnrollmentId getId() {
+        return id;
+    }
+
+    public void setId(EnrollmentId id) {
+        this.id = id;
+    }
+
+    public Student getStudent() {
+        return student;
+    }
+
+    public void setStudent(Student student) {
+        this.student = student;
+    }
+
+    public Course getCourse() {
+        return course;
+    }
+
+    public void setCourse(Course course) {
+        this.course = course;
+    }
+}
