7b026c2c8afd719cf4c5668cf058e25e108f11b7
Gabriela Carvalho Camilo <gabccamilo.dev@gmail.com>
2021-01-06 00:21:31 -0300

    Adding Application execution to call implemented functionalities

--- src/main/java/com/example/demo/Application.java
+++ src/main/java/com/example/demo/Application.java
@@ -6,6 +6,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @SpringBootApplication
@@ -15,15 +16,13 @@ public class Application {
         SpringApplication.run(Application.class, args);
     }
 
-    /*Code running after the application started*/
     @Bean
-    CommandLineRunner commandLineRunner(StudentRepository studentRepository) {
-        return args -> generateRandomStudents(studentRepository);
-    }
+    CommandLineRunner commandLineRunner(
+            StudentRepository studentRepository,
+            StudentIdCardRepository studentIdCardRepository) {
+        return args -> {
+            Faker faker = new Faker();
 
-    private void generateRandomStudents(StudentRepository studentRepository) {
-        Faker faker = new Faker();
-        for (int i = 0; i < 20; i++) {
             String firstName = faker.name().firstName();
             String lastName = faker.name().lastName();
             String email = String.format("%s.%s@amigoscode.edu", firstName, lastName);
@@ -32,8 +31,57 @@ public class Application {
                     lastName,
                     email,
                     faker.number().numberBetween(17, 55));
-            studentRepository.save(student);
-        }
-    }
 
+            student.addBook(
+                    new Book("Clean Code", LocalDateTime.now().minusDays(4)));
+
+            student.addBook(
+                    new Book("Think and Grow Rich", LocalDateTime.now()));
+
+            student.addBook(
+                    new Book("Spring Data JPA", LocalDateTime.now().minusYears(1)));
+
+            StudentIdCard studentIdCard = new StudentIdCard(
+                    "123456789",
+                    student);
+
+            student.setStudentIdCard(studentIdCard);
+
+            student.addEnrollment(new Enrollment(
+                    new EnrollmentId(1L, 1L),
+                    student,
+                    new Course("Computer Science", "IT"),
+                    LocalDateTime.now()
+            ));
+
+            student.addEnrollment(new Enrollment(
+                    new EnrollmentId(1L, 2L),
+                    student,
+                    new Course("Amigoscode Spring Data JPA", "IT"),
+                    LocalDateTime.now().minusDays(18)
+            ));
+
+            student.addEnrollment(new Enrollment(
+                    new EnrollmentId(1L, 2L),
+                    student,
+                    new Course("Amigoscode Spring Data JPA", "IT"),
+                    LocalDateTime.now().minusDays(18)
+            ));
+
+
+
+            studentRepository.save(student);
+
+            studentRepository.findById(1L)
+                    .ifPresent(s -> {
+                        System.out.println("fetch book lazy...");
+                        List<Book> books = student.getBooks();
+                        books.forEach(book -> {
+                            System.out.println(
+                                    s.getFirstName() + " borrowed " + book.getBookName());
+                        });
+                    });
+
+        };
+    }
 }
